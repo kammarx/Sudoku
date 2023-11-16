0dccd9f0d239e1ad10b8f9013e74174d99e5b789
kammar <kammarufa6@gmail.com>
2023-11-16 19:18:34 +0100

    class SudokuBoard implemented

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
+        SudokuBoard board = new SudokuBoard();
+        board.displayBoard();
 
     }
 }
--- src/com/company/SudokuBoard.java
+++ src/com/company/SudokuBoard.java
@@ -1,6 +1,47 @@
 package com.company;
 
 public class SudokuBoard {
-   
+    private int[][] board;
+
+    public SudokuBoard(){
+        board = new int[9][9];
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                board[i][j]=0;
+            }
+        }
+    }
+
+    public void fillBoard(int[][] initialValues){
+        if(initialValues.length != 9 || initialValues[0].length !=9){
+            System.out.println("Invalid input for initial values. The array must be 9x9.");
+            return;
+        }
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                board[i][j] = initialValues[i][j];
+            }
+        }
+
+    }
+
+    public void displayBoard(){
+        System.out.println("----------------------");
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if(j%3==0) System.out.print("|");
+                System.out.print(board[i][j] + " ");
+            }
+
+            System.out.println("|");
+
+            if (i == 8) {
+                System.out.println("----------------------");
+            } else if ((i + 1) % 3 == 0) {
+                System.out.println("|--------------------|");
+            }
+        }
+    }
 
 }
